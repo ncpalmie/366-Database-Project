b3a4f784580f2420da0427f76e1e9885080e1ce2
Suhas Panthari <spanthar@calpoly.edu>
2020-06-12 08:13:38 -0700

    Update LocMgrRepository.java

--- src/main/java/csc366/jpademo/LocMgrRepository.java
+++ src/main/java/csc366/jpademo/LocMgrRepository.java
@@ -18,6 +18,9 @@ public interface LocMgrRepository extends JpaRepository<LocMgr, Long>{
     @Query("from LocMgr o where o.firstName = :name or o.lastName = :name")
     LocMgr findByNameJpql(@Param("name") String name);
     
+    @Query("select o from LocMgr o join o.stores store where o.firstName = :name or o.lastName = :name")
+    LocMgr findByNameWithStoreJpql(@Param("name") String name);
+    
     // Native SQL query
     @Query(value = "select * from LocMgr as o where o.first_name = :name or o.last_name = :name", nativeQuery = true)
     LocMgr findByNameSql(@Param("name") String name);
