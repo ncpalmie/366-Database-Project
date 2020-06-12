b446ee14e779cab2360cc768afd0cb95851d5dad
Suhas Panthari <spanthar@calpoly.edu>
2020-06-12 09:00:54 -0700

    Create LocMgrTests.java

---
+++ src/test/java/csc366/jpademo/LocMgrTests.java
@@ -0,0 +1,110 @@
+package csc366.jpademo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.TestPropertySource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// locMgrTests: Add, list, and remove LocMgr instances
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@TestPropertySource(properties = {
+	"spring.main.banner-mode=off",
+	"spring.jpa.hibernate.ddl-auto=update",
+	"logging.level.root=ERROR",
+	"logging.level.csc366=DEBUG",
+
+	"logging.level.org.hibernate.SQL=DEBUG",
+	"logging.level.org.hibernate.type.descriptor.sql.BasicBinder=TRACE", // display prepared statement parameters
+	"spring.jpa.properties.hibernate.format_sql=true",
+	"spring.jpa.show-sql=false",   // prevent duplicate logging
+	"spring.jpa.properties.hibernate.show_sql=false",	
+	
+	"logging.pattern.console= %d{yyyy-MM-dd HH:mm:ss} - %msg%n"
+})
+@TestMethodOrder(OrderAnnotation.class)
+public class locMgrTests {
+
+    private final static Logger log = LoggerFactory.getLogger(locMgrTests.class);
+    
+    @Autowired
+    private LocMgrRepository locMgrRepository;
+
+    private final LocMgr locMgr = new locMgr("test", "test", "test@calpoly.edu", "123-45-6789", "02/02/77", "123-456-7890");
+    
+    @BeforeEach
+    private void setup() {
+	locMgrRepository.saveAndFlush(locMgr);
+    }
+    
+    @Test
+    @Order(1)
+    public void testSaveLocMgr() {
+	LocMgr logMgr2 = logMgrRepository.findByFirstName("test");
+
+	log.info(logMgr2.toString());
+	
+	assertNotNull(locMgr);
+	assertEquals(locMgr2.getFirstName(), locMgr.getFirstName());
+	assertEquals(locMgr2.getLastName(), locMgr.getLastName());
+    }
+    
+    @Test
+    @Order(2)
+    public void testGetLocMgr() {
+	LocMgr locMgr2 = locMgrRepository.findByFirstName("test");
+	assertNotNull(locMgr);
+	assertEquals(locMgr2.getFirstName(), locMgr.getFirstName());
+	assertEquals(locMgr2.getLastName(), locMgr.getLastName());
+    }
+
+    @Test
+    @Order(3)
+    public void testDeleteLocMgr() {
+	locMgrRepository.delete(locMgr);
+	locMgrRepository.flush();
+    }
+    
+    @Test
+    @Order(4)
+    public void testFindAllLocMgrs() {
+	assertNotNull(locMgrRepository.findAll());
+    }
+    
+    @Test
+    @Order(5)
+    public void testDeleteByLocMgrId() {
+	LocMgr e = locMgrRepository.findByFirstName("test");
+	locMgrRepository.deleteById(e.getId());
+	locMgrRepository.flush();
+    }
+
+    @Test
+    @Order(6)
+    public void testJpqlFinder() {
+	LocMgr e = locMgrRepository.findByNameJpql("test");
+	assertEquals(e.getFirstName(), locMgr.getFirstName());
+    }
+
+    @Test
+    @Order(7)
+    public void testSqlFinder() {
+	LocMgr p = locMgrRepository.findByNameSql("test");
+	assertEquals(p.getFirstName(), locMgr.getFirstName());
+    }
+
+}
